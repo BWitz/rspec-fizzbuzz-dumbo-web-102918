a958a4c174af9426cf846f2c8501520687278651
Brian Horowitz <bhorowitz2014@gmail.com>
2018-10-09 06:34:36 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -6,7 +6,7 @@ def fizzbuzz(int)
     return "FizzBuzz"
   if int % 3 == 0 && int % 5 != 0
     return "Fizz"
-  if int % 3 != 0 && int % 5 == 0
+  if int % 5 == 0 && int % 3 != 0
     return "Buzz"
   if int % 3 != 0 && int % 5 != 0
     return nil
